99b005f56df71e62219ca45364abf2b5037106da
foxzihel17 <clydejustin1117@gmail.com>
2024-06-06 11:26:02 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,66 @@
-const express = require('express');
+/**  const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./apiRoutes.json');
+
+//*Routes for Api's Files
 const gttsRouter = require('./routes/gtts');
-//const gpt-goRouter = require('./routes/gpt-go');
 const animeQuoteRouter = require('./routes/animequotes');
 const brainlyRouter = require('./routes/brainly');
 const gsmarenaRouter = require('./routes/gsmarena');
+const lyricsRouter = 
+require('./routes/lyrics');
+const animeRouter = 
+require('./routes/anime');
+const gscraperRouter =
+require('./routes/gscraper');
+const scraperRouter = 
+require('./routes/scraper');
+const ytsearchRouter =
+require('./routes/ytsearch');
+const ocrRouter = 
+require('./routes/ocr');
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// JSON middleware
+app.use(express.json());
+
+// Serve Swagger UI documentation
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Route to serve REST API homepage
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/homepage.html');
+});
+
+// Routes for APIs
+app.use('/gtts', gttsRouter);
+app.use('/animequote', animeQuoteRouter);
+app.use('/brainly', brainlyRouter);
+app.use(`/gsmarena`, gsmarenaRouter);
+app.use('/lyrics', lyricsRouter);
+app.use('/anime', animeRouter);
+app.use('/gscraper', gscraperRouter);
+app.use('/scraper', scraperRouter);
+app.use('/ytsearch' ,ytsearchRouter);
+app.use('/ocr' , ocrRouter);
+
+
+
+// Start server
+app.listen(port, () => {
+  console.log(`API listening at http://localhost:${port}`);
+});
+*/
+const express = require('express');
+const swaggerDocument = require('./apiRoutes.json');
+const gttsRouter = require('./routes/gtts');
+const animeQuoteRouter = require('./routes/animequotes');
+const brainlyRouter = require('./routes/brainly');
+const gsmarenaRouter = require('./routes/gsmarena');
+const lyricsRouter = require('./routes/lyrics');
 const animeRouter = require('./routes/anime');
 const gscraperRouter = require('./routes/gscraper');
 const scraperRouter = require('./routes/scraper');
@@ -24,12 +80,12 @@ app.use('/gtts', gttsRouter);
 app.use('/animequote', animeQuoteRouter);
 app.use('/brainly', brainlyRouter);
 app.use('/gsmarena', gsmarenaRouter);
+app.use('/lyrics', lyricsRouter);
 app.use('/anime', animeRouter);
 app.use('/gscraper', gscraperRouter);
 app.use('/scraper', scraperRouter);
 app.use('/ytsearch', ytsearchRouter);
 app.use('/ocr', ocrRouter);
-//app.use('/gpt-go', gpt-goRouter);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/homepage.html');
